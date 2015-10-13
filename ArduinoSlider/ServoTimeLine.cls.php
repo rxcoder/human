85fa5e04b4188c5ab19ebf7c7944bd598b6e4493
Andrey Fadeev <a.fadeev@speereo.com>
2015-10-13 15:49:51 +0300

    first commit from work. ArduinoSlider folder

---
+++ ArduinoSlider/ServoTimeLine.cls.php
@@ -0,0 +1,149 @@
+<?php
+
+
+
+// распределение скорости между фреймами
+define ("STL_SPEED_NORMAL", 0);// нормальная скорость
+define ("STL_SPEED_ACCELERATION", 1);// ускорение
+define ("STL_SPEED_DECELERATION", 2);// замедление
+
+// ускорение или замедление идёт линейно или по синусоиде.
+define ("STL_MOVE_LINEARY", 0);// ускорение/замедление - линейно
+define ("STL_MOVE_FAST", 1);// ускорение/замедление - быстро
+define ("STL_MOVE_SLOW", 2);// ускорение/замедление - медленно
+
+
+
+
+class servo_modelBlank // чистая модель поведенья
+{
+	var $servos=array(// 12 servos
+    	"s1"=>array(),
+    	"s2"=>array(),
+    	"s3"=>array(),
+    	"s4"=>array(),
+    	"s5"=>array(),
+    	"s6"=>array(),
+    	"s7"=>array(),
+    	"s8"=>array(),
+    	"s9"=>array(),
+    	"s10"=>array(),
+    	"s11"=>array(),
+    	"s12"=>array()
+    );
+
+    var $timeWork=0;// общее время работы скрипта timeLine  миллисекунды
+    var $timeFrame=0;// длительность одного кадра           миллисекунды
+    var $repeats=0;// количество повторов
+
+    public function addKeyFrame($frmPack){
+        // раскидываем полученные фреймы по местам
+		foreach($this->servos as $name=>$data){
+            if(is_array($frmPack[$name])){
+                for($i=0;$i<sizeof($frmPack[$name]);$i++){
+                    $this->servos[$name][]=$frmPack[$name][$i];
+                }
+            }
+		}
+    }
+}
+
+
+
+class rxServoTimeLine // перемещение по временной шкале используя модель поведения
+{
+
+	public $behaviorModels = array();// модель поведения
+	public $arrServos=array("s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12");
+	public $servoKeys=array();// массив времени исполнения кадра
+	public $servoTimeline=array();// позиция серв в определённый момент времени.
+
+
+
+
+	public function addModel($model)
+	{
+	    //$model=$this->calculateTimeLine($model);
+        $this->behaviorModels[]=$model;
+
+	}
+
+    // посчитать позицию для всех кадров во всех моделях
+	public function calculateTimeLine()
+	{
+        $globTime=0;
+        $total=array();
+        $this->servoKeys=array();
+	    for($j=0;$j<sizeof($this->behaviorModels);$j++){// цикл по моделям
+	    	$model=$this->behaviorModels[$j];
+	    	$modelRepeat=$model->repeats;
+
+	    	do{// когда 1 модель повторяется неск. раз
+
+	    	    for($k=0;$k<sizeof($this->arrServos);$k++){// перебираем сервы
+                    $name=$this->arrServos[$k];
+		            if(sizeof($model->servos[$name])>0){// если для данного сервы есть модель поведения...
+		                $frames=$model->servos[$name];// массив кадров одной сервы
+                        for($i=0;$i<=$model->timeWork;$i+=$model->timeFrame){
+                            $angle=$this->calcFrame($i, $frames);
+                            $tm=$globTime+$i;
+                            $total[$tm][$name]=$angle;
+                            if(!in_array($tm, $this->servoKeys))$this->servoKeys[]=$tm;
+                        }
+		            }
+		        }
+		        $modelRepeat--;
+            	$globTime+=$model->timeWork;
+	        }while($modelRepeat>=0);
+
+        }
+
+        $this->servoTimeline=$total;
+	}
+
+    public function calcFrame($time, $arrKeyFrame)
+	{
+	    $from=0;
+	    $to=999999999;
+	    $fromCnt=0;
+	    $toCnt=0;
+	    for($i=0;$i<sizeof($arrKeyFrame);$i++){
+            if(($arrKeyFrame[$i]["time"]<=$time)&&($arrKeyFrame[$i]["time"]>=$from)){
+            	$from=$arrKeyFrame[$i]["time"];
+            	$fromCnt=$i;
+            }
+            if(($arrKeyFrame[$i]["time"]>=$time)&&($arrKeyFrame[$i]["time"]<=$to)){
+            	$to=$arrKeyFrame[$i]["time"];
+            	$toCnt=$i;
+            }
+		}
+		if($to==0){// после последнего фрейма
+		    $to=$from;
+		}
+
+        if($from == $to)return $arrKeyFrame[$fromCnt]["angle"];
+
+        if($arrKeyFrame[$fromCnt]["speed"]==STL_SPEED_NORMAL){// обычная скорость без ускорений/замедлений
+        	$a=$arrKeyFrame[$fromCnt]["angle"] + ($time-$from) * ($arrKeyFrame[$toCnt]["angle"]-$arrKeyFrame[$fromCnt]["angle"]) / ($to-$from);
+        }
+        else if($arrKeyFrame[$fromCnt]["speed"]==STL_SPEED_ACCELERATION){// ускорение
+            $b=($time-$from)/($to-$from);// множитель линейно
+            $b=1+cos( PI()+($b*(PI()/2)));// множитель
+            $a=($arrKeyFrame[$toCnt]["angle"]-$arrKeyFrame[$fromCnt]["angle"]);
+            $a=$arrKeyFrame[$fromCnt]["angle"] +  $a*$b;
+        }
+        else if($arrKeyFrame[$fromCnt]["speed"]==STL_SPEED_DECELERATION){// замедление
+            $b=($time-$from)/($to-$from);// множитель линейно
+            $b=sin($b*(PI()/2));// множитель
+            $a=($arrKeyFrame[$toCnt]["angle"]-$arrKeyFrame[$fromCnt]["angle"]);
+            $a=$arrKeyFrame[$fromCnt]["angle"] +  $a*$b;
+        }
+        return  "$a";
+    }
+}
+
+
+
+
+
+?>
---
+++ ArduinoSlider/index.php
@@ -0,0 +1,105 @@
+<?php
+
+
+include_once("ServoTimeLine.cls.php");
+$tl=new rxServoTimeLine();
+
+
+// первая модель передвижения----------------------------------------------------------------
+
+$frmPack=array(
+	"s1"=>array(
+	    array(// 1й кадр
+	    	"angle"=>0,
+	    	"time"=>0,
+	    	"speed"=>STL_SPEED_ACCELERATION
+	    ),
+	    array(// 2й кадр
+	    	"angle"=>45,
+	    	"time"=>1000,
+	    	"speed"=>STL_SPEED_NORMAL
+	    ),
+	    array(// 3й кадр
+	    	"angle"=>135,
+	    	"time"=>4000,
+	    	"speed"=>STL_SPEED_DECELERATION
+	    ),
+	    array(// 4й кадр
+	    	"angle"=>180,
+	    	"time"=>5000,
+	    	"speed"=>STL_SPEED_NORMAL
+	    )
+	),
+	"s2"=>array(
+	    array(// 1й кадр
+	    	"angle"=>0,
+	    	"time"=>0,
+	    	"speed"=>STL_SPEED_NORMAL
+	    ),
+	    array(// 4й кадр
+	    	"angle"=>180,
+	    	"time"=>5000,
+	    	"speed"=>STL_SPEED_NORMAL
+	    )
+	)
+);
+
+// создать модель поведения на основе созданных кадров
+$model = new servo_modelBlank();
+$model->timeWork=5000;
+$model->timeFrame=200;
+$model->repeats=1;
+$model->addKeyFrame($frmPack);
+$tl->addModel($model);
+
+
+
+// вторая модель передвижения----------------------------------------------------------------
+
+$frmPack2=array(
+	"s2"=>array(
+	    array(// 1й кадр
+	    	"angle"=>0,
+	    	"time"=>0,
+	    	"speed"=>STL_SPEED_NORMAL
+	    ),
+	    array(// 4й кадр
+	    	"angle"=>180,
+	    	"time"=>2000,
+	    	"speed"=>STL_SPEED_NORMAL
+	    )
+	)
+);
+$model2 = new servo_modelBlank();
+$model2->timeWork=2000;
+$model2->timeFrame=100;
+$model2->repeats=0;
+$model2->addKeyFrame($frmPack2);
+$tl->addModel($model2);
+
+
+
+// подсчитать модели - разложить их по таймлайну.
+$tl->calculateTimeLine();
+
+$time=0;
+$time2=0;
+
+for($i=0;$i<sizeof($tl->servoKeys);$i++){
+    // пауза между фреймами
+    $time=$tl->servoKeys[$i];
+    if($time2>0)usleep((int) ($time-$time2 * 1000));
+    $time2=$time;
+    $arrPos=$tl->$servoTimeline[$tl->servoKeys[$i]];
+
+    if($arrPos["s1"]!=''){}// $arrPos["s1"]=угол
+    if($arrPos["s2"]!=''){}// $arrPos["s2"]=угол
+    //...
+    if($arrPos["s12"]!=''){}// $arrPos["s12"]=угол
+
+
+}
+
+
+
+?>
